07eb67f65fdd7aae6d31cecef617897aa3b1c349
Carlos Mario Martínez <carlos.mario.m@outlook.com>
2022-11-26 19:35:04 -0500

    add edit proy component content

--- src/app/components/proys/edit-proy/edit-proy.component.ts
+++ src/app/components/proys/edit-proy/edit-proy.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { proyects } from 'src/app/shared/models/proyect';
 
 @Component({
   selector: 'app-edit-proy',
@@ -7,9 +9,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditProyComponent implements OnInit {
 
-  constructor() { }
+  //Llamamos los proyectos
+  proyects = proyects
+  //Inicializamos el título y la descripción
+  title = ""
+  description = ""
+  constructor(private router: Router) { }
 
-  ngOnInit(): void {
+  //Funciones que se ejecutan al inicio
+  ngOnInit() {
+    //Se obtiene el resultado
+    const result = this.getProy();
+    //Se asignan título y descripción
+    this.title = result.title;
+    this.description = result.description;
+  }
+
+  getProy(): any {
+    //Se obtiene la url completa y se modifica para obtener el final
+    const a = this.router.url.split('/').pop();
+    //Se busca el proyecto que tenga la id especificada
+    let result = proyects.find(item => item.id === a);
+    //Retorna el resultado
+    return result
   }
 
 }
