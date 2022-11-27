d0c22d3c6a6dcc3076f673b94d24ebb8352d324f
Carlos Mario Martínez <carlos.mario.m@outlook.com>
2022-11-26 19:34:32 -0500

    add proys id filter

--- src/app/components/proys/list-proys/list-proys.component.ts
+++ src/app/components/proys/list-proys/list-proys.component.ts
@@ -10,7 +10,22 @@ export class ListProysComponent implements OnInit {
 
   constructor() { }
   proyects = proyects
+  id = "";
+  //Llamamos todos los items
+  proyects0 = proyects
+  //Cantidad total de items
+  cantidadTotal = proyects.length;
+  //Cantidad de items por página
+  cantidadPag = 5;
+  //Creamos el array inicial
+  proyects1 = this.proyects0.slice(0, this.cantidadPag);
   ngOnInit(): void {
+  }
+  paginate(event: any) {
+    /*Si el evento de cambiar página sucede, al array inicial le agregamos un slice con el primer elemento que debería
+    más el total*/
+    this.proyects1 = this.proyects0.slice(event.first, this.cantidadPag + event.first)
+    console.log(event.rows)
   }
 
 }
--- src/app/components/proys/list-proys/list-proys.module.ts
+++ src/app/components/proys/list-proys/list-proys.module.ts
@@ -1,17 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { ListProysRoutingModule } from './list-proys-routing.module';
 import { ListProysComponent } from './list-proys.component';
+import { PaginatorModule } from 'primeng/paginator';
+import { TableFilterPipe } from '../../proys/list-proys/proy-filter.pipe';
 
 
 @NgModule({
   declarations: [
-    ListProysComponent
+    ListProysComponent, TableFilterPipe
   ],
   imports: [
     CommonModule,
-    ListProysRoutingModule
+    ListProysRoutingModule, PaginatorModule
   ]
 })
 export class ListProysModule { }
