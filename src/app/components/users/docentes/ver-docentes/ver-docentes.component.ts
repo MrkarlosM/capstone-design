e5cd63359f06a1c7de98c3cf619499b738772d45
Carlos Mario Martínez <carlos.mario.m@outlook.com>
2022-12-11 16:10:09 -0500

    add  tipoUser

--- src/app/components/users/docentes/ver-docentes/ver-docentes.component.ts
+++ src/app/components/users/docentes/ver-docentes/ver-docentes.component.ts
@@ -3,7 +3,7 @@ import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { FormControl } from '@angular/forms';
 import { map, Observable } from 'rxjs';
 import { Docente, docentes } from 'src/app/shared/models/docente';
-import { DocentesService } from './docentes.service';
+import { DocentesService } from '../docentes.service';
 
 @Component({
   selector: 'app-ver-docentes',
@@ -22,27 +22,28 @@ export class VerDocentesComponent implements OnInit {
   //Creamos el array inicial
   //docentes1 = this.docentes0.slice(0, this.cantidadPag);
   docentes: any[] | undefined;
+  tipoUser = 'PROFESORES'
   constructor(public firestore: AngularFirestore, private userSvc: DocentesService) {
   }
 
   ngOnInit() {
     //Los usuarios para paginar
-    this.userSvc.getUsersPaginate('PROFESORES',this.cantidadPag).subscribe(res=>this.docentes=res);
+    this.userSvc.getUsersPaginate(this.tipoUser,this.cantidadPag).subscribe(res=>this.docentes=res);
     //Los usuarios totales
-    this.userSvc.getAllUsers('PROFESORES').subscribe(res=>this.cantidadTotal=res.length);
+    this.userSvc.getAllUsers(this.tipoUser).subscribe(res=>this.cantidadTotal=res.length);
   }
 
   paginate(event: any) {
     /*Si el evento de cambiar página sucede, al array inicial le agregamos un slice con el primer elemento que debería
     más el total*/
     //this.docentes1 = this.docentes0.slice(event.first, this.cantidadPag + event.first)
-    this.userSvc.getAllUsers('PROFESORES').subscribe(res=>this.docentes = res.slice(event.first, this.cantidadPag + event.first));
+    this.userSvc.getAllUsers(this.tipoUser).subscribe(res=>this.docentes = res.slice(event.first, this.cantidadPag + event.first));
   }
 
   searchName() {
-    this.userSvc.getUsersSearch('PROFESORES',this.name.value).subscribe(res=>this.docentes=res);
+    this.userSvc.getUsersSearch(this.tipoUser,this.name.value).subscribe(res=>this.docentes=res);
   }
   clear() {
-    this.userSvc.getUsersPaginate('PROFESORES',this.cantidadPag).subscribe(res=>this.docentes=res);
+    this.userSvc.getUsersPaginate(this.tipoUser,this.cantidadPag).subscribe(res=>this.docentes=res);
   }
 }
--- src/app/components/users/estudiantes/ver-estudiantes/ver-estudiantes.component.ts
+++ src/app/components/users/estudiantes/ver-estudiantes/ver-estudiantes.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
-import { DocentesService } from '../../docentes/ver-docentes/docentes.service';
+import { EstudiantesService } from '../estudiantes.service';
 
 @Component({
   selector: 'app-ver-estudiantes',
@@ -9,13 +9,14 @@ import { DocentesService } from '../../docentes/ver-docentes/docentes.service';
 })
 export class VerEstudiantesComponent implements OnInit {
 
-  constructor(private userSvc: DocentesService) { 
+  constructor(private userSvc: EstudiantesService) { 
   }
 
   name = new FormControl('');
   cantidadTotal = 0;
   cantidadPag = 5;
   estudiantes: any[] | undefined;
+  tipoUser = 'ESTUDIANTES'
   //Llamamos todos los items
   // estudiantes0 = estudiantes
   // //Cantidad total de items
@@ -28,21 +29,21 @@ export class VerEstudiantesComponent implements OnInit {
   ngOnInit() {
     //Los usuarios para paginar
     //Los usuarios para paginar
-    this.userSvc.getUsersPaginate('ESTUDIANTES',this.cantidadPag).subscribe(res=>this.estudiantes=res);
+    this.userSvc.getUsersPaginate(this.tipoUser,this.cantidadPag).subscribe(res=>this.estudiantes=res);
     //Los usuarios totales
-    this.userSvc.getAllUsers('ESTUDIANTES').subscribe(res=>this.cantidadTotal=res.length);
+    this.userSvc.getAllUsers(this.tipoUser).subscribe(res=>this.cantidadTotal=res.length);
   }
   paginate(event: any) {
     /*Si el evento de cambiar página sucede, al array inicial le agregamos un slice con el primer elemento que debería
     más el total*/
     //this.docentes1 = this.docentes0.slice(event.first, this.cantidadPag + event.first)
-    this.userSvc.getAllUsers('PROFESORES').subscribe(res=>this.estudiantes = res.slice(event.first, this.cantidadPag + event.first));
+    this.userSvc.getAllUsers(this.tipoUser).subscribe(res=>this.estudiantes = res.slice(event.first, this.cantidadPag + event.first));
   }
 
   searchName() {
-    this.userSvc.getUsersSearch('PROFESORES',this.name.value).subscribe(res=>this.estudiantes=res);
+    this.userSvc.getUsersSearch(this.tipoUser,this.name.value).subscribe(res=>this.estudiantes=res);
   }
   clear() {
-    this.userSvc.getUsersPaginate('PROFESORES',this.cantidadPag).subscribe(res=>this.estudiantes=res);
+    this.userSvc.getUsersPaginate(this.tipoUser,this.cantidadPag).subscribe(res=>this.estudiantes=res);
   }
 }
