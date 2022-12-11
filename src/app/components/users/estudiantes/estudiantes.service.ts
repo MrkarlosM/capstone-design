be38ba5bf3a05c6951b162939441e0ee5c06723d
Carlos Mario Martínez <carlos.mario.m@outlook.com>
2022-12-11 16:09:33 -0500

    create estudiantesService

---
+++ src/app/components/users/estudiantes/estudiantes.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { map, Observable } from 'rxjs';
+import { Estudiante } from 'src/app/shared/models/estudiante';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EstudiantesService {
+
+  constructor(private afs: AngularFirestore) {   }
+  //Obtiene todos los items de una colección dado el nombre de la colección de forma ascendente por su name
+  public getAllUsers(user: string):Observable<any[]>{
+    return this.afs.collection(user, ref => ref.orderBy('name', 'asc')).snapshotChanges().pipe(map(actions=>actions.map(a =>{
+      const data = a.payload.doc.data() as Estudiante;
+      const id = a.payload.doc.id;
+      return {id, ... data}
+    })))
+  }
+
+  //Obtiene todos los items de una colección dado el nombre de la colección de forma ascendente por su name hasa el limit
+  public getUsersPaginate(user: string, limit: number):Observable<any[]>{
+    return this.afs.collection(user, ref => ref.orderBy('name', 'asc').limit(limit)).snapshotChanges().pipe(map(actions=>actions.map(a =>{
+      const data = a.payload.doc.data() as Estudiante;
+      const id = a.payload.doc.id;
+      return {id, ... data}
+    })))
+  }
+  //Buca todos los items de una colección dado el nombre de la colección
+  public getUsersSearch(user: string, name: string | null):Observable<any[]>{
+    return this.afs.collection(user, ref => ref.where('name', 'in', [name])).snapshotChanges().pipe(map(actions=>actions.map(a =>{
+      const data = a.payload.doc.data() as Estudiante;
+      const id = a.payload.doc.id;
+      return {id, ... data}
+    })))
+  }
+
+}
