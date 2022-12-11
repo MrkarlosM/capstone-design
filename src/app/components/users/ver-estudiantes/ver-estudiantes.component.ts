631912aa8495ac71561bcd491fdce1cd06dc71ac
Carlos Mario Martínez <carlos.mario.m@outlook.com>
2022-12-11 15:51:06 -0500

    add students pagination from firebase

--- src/app/components/users/ver-estudiantes/ver-estudiantes.component.ts
+++ src/app/components/users/ver-estudiantes/ver-estudiantes.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { AngularFirestore } from '@angular/fire/compat/firestore';
-import { Observable } from 'rxjs';
-import { estudiantes } from 'src/app/shared/models/estudiante';
+import { FormControl } from '@angular/forms';
+import { UsersService } from '../users.service';
 
 @Component({
   selector: 'app-ver-estudiantes',
@@ -9,27 +8,41 @@ import { estudiantes } from 'src/app/shared/models/estudiante';
   styleUrls: ['./ver-estudiantes.component.css']
 })
 export class VerEstudiantesComponent implements OnInit {
-  items: Observable<any[]>;
-  constructor(firestore: AngularFirestore) { 
-    this.items = firestore.collection('ESTUDIANTES').valueChanges();
+
+  constructor(private userSvc: UsersService) { 
   }
 
-  name = "";
-  //Llamamos todos los items
-  estudiantes0 = estudiantes
-  //Cantidad total de items
-  cantidadTotal = estudiantes.length;
-  //Cantidad de items por página
+  name = new FormControl('');
+  cantidadTotal = 0;
   cantidadPag = 5;
+  estudiantes: any[] | undefined;
+  //Llamamos todos los items
+  // estudiantes0 = estudiantes
+  // //Cantidad total de items
+  // cantidadTotal = estudiantes.length;
+  // //Cantidad de items por página
+  // cantidadPag = 5;
   //Creamos el array inicial
-  estudiantes1 = this.estudiantes0.slice(0, this.cantidadPag);
+  //estudiantes1 = this.estudiantes0.slice(0, this.cantidadPag);
 
-  ngOnInit(): void {
+  ngOnInit() {
+    //Los usuarios para paginar
+    //Los usuarios para paginar
+    this.userSvc.getUsersPaginate('ESTUDIANTES',this.cantidadPag).subscribe(res=>this.estudiantes=res);
+    //Los usuarios totales
+    this.userSvc.getAllUsers('ESTUDIANTES').subscribe(res=>this.cantidadTotal=res.length);
   }
   paginate(event: any) {
     /*Si el evento de cambiar página sucede, al array inicial le agregamos un slice con el primer elemento que debería
     más el total*/
-    this.estudiantes1 = this.estudiantes0.slice(event.first, this.cantidadPag + event.first)
-    console.log(event.rows)
+    //this.docentes1 = this.docentes0.slice(event.first, this.cantidadPag + event.first)
+    this.userSvc.getAllUsers('PROFESORES').subscribe(res=>this.estudiantes = res.slice(event.first, this.cantidadPag + event.first));
+  }
+
+  searchName() {
+    this.userSvc.getUsersSearch('PROFESORES',this.name.value).subscribe(res=>this.estudiantes=res);
+  }
+  clear() {
+    this.userSvc.getUsersPaginate('PROFESORES',this.cantidadPag).subscribe(res=>this.estudiantes=res);
   }
 }
