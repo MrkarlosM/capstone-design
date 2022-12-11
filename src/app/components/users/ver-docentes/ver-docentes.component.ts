38829a23c831ee2ea2f3bc7965469c33db6ad3ac
Carlos Mario Martínez <carlos.mario.m@outlook.com>
2022-12-11 15:39:31 -0500

    add pagination docentes

--- src/app/components/users/ver-docentes/ver-docentes.component.ts
+++ src/app/components/users/ver-docentes/ver-docentes.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
-import { Observable } from 'rxjs';
-import { docentes } from 'src/app/shared/models/docente';
+import { FormControl } from '@angular/forms';
+import { map, Observable } from 'rxjs';
+import { Docente, docentes } from 'src/app/shared/models/docente';
+import { UsersService } from '../users.service';
 
 @Component({
   selector: 'app-ver-docentes',
@@ -10,28 +12,37 @@ import { docentes } from 'src/app/shared/models/docente';
 })
 export class VerDocentesComponent implements OnInit {
 
-  name = "";
+  name = new FormControl('');
   //Llamamos todos los items
-  docentes0 = docentes
+  //docentes0 = docentes
   //Cantidad total de items
-  cantidadTotal = docentes.length;
+  cantidadTotal = 0;
   //Cantidad de items por página
   cantidadPag = 5;
   //Creamos el array inicial
-  docentes1 = this.docentes0.slice(0, this.cantidadPag);
-  items: Observable<any[]>;
-  constructor(firestore: AngularFirestore) { 
-    this.items = firestore.collection('PROFESORES').valueChanges();
+  //docentes1 = this.docentes0.slice(0, this.cantidadPag);
+  docentes: any[] | undefined;
+  constructor(public firestore: AngularFirestore, private userSvc: UsersService) {
   }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    //Los usuarios para paginar
+    this.userSvc.getUsersPaginate('PROFESORES',this.cantidadPag).subscribe(res=>this.docentes=res);
+    //Los usuarios totales
+    this.userSvc.getAllUsers('PROFESORES').subscribe(res=>this.cantidadTotal=res.length);
   }
+
   paginate(event: any) {
     /*Si el evento de cambiar página sucede, al array inicial le agregamos un slice con el primer elemento que debería
     más el total*/
     //this.docentes1 = this.docentes0.slice(event.first, this.cantidadPag + event.first)
-    console.log(event.rows)
+    this.userSvc.getAllUsers('PROFESORES').subscribe(res=>this.docentes = res.slice(event.first, this.cantidadPag + event.first));
   }
 
-
+  searchName() {
+    this.userSvc.getUsersSearch('PROFESORES',this.name.value).subscribe(res=>this.docentes=res);
+  }
+  clear() {
+    this.userSvc.getUsersPaginate('PROFESORES',this.cantidadPag).subscribe(res=>this.docentes=res);
+  }
 }
