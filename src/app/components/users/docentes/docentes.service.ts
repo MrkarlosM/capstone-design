80a11854f9c1c3ed62c1d439813d7e25ccd5fbe6
Carlos Mario Martínez <carlos.mario.m@outlook.com>
2022-12-11 21:01:38 -0500

    deploy to hosting

--- src/app/components/users/docentes/docentes.service.ts
+++ src/app/components/users/docentes/docentes.service.ts
@@ -8,41 +8,45 @@ import { Docente } from 'src/app/shared/models/docente';
 })
 export class DocentesService {
 
-  constructor(private afs: AngularFirestore) {   }
+  constructor(private afs: AngularFirestore) { }
   //Obtiene todos los items de una colección dado el nombre de la colección de forma ascendente por su name
-  public getAllUsers(user: string):Observable<any[]>{
-    return this.afs.collection(user, ref => ref.orderBy('name', 'asc')).snapshotChanges().pipe(map(actions=>actions.map(a =>{
+  public getAllUsers(user: string): Observable<any[]> {
+    return this.afs.collection(user, ref => ref.orderBy('name', 'asc')).snapshotChanges().pipe(map(actions => actions.map(a => {
       const data = a.payload.doc.data() as Docente;
       const id = a.payload.doc.id;
-      return {id, ... data}
+      return { id, ...data }
     })))
   }
 
   //Obtiene todos los items de una colección dado el nombre de la colección de forma ascendente por su name hasa el limit
-  public getUsersPaginate(user: string, limit: number):Observable<any[]>{
-    return this.afs.collection(user, ref => ref.orderBy('name', 'asc').limit(limit)).snapshotChanges().pipe(map(actions=>actions.map(a =>{
+  public getUsersPaginate(user: string, limit: number): Observable<any[]> {
+    return this.afs.collection(user, ref => ref.orderBy('name', 'asc').limit(limit)).snapshotChanges().pipe(map(actions => actions.map(a => {
       const data = a.payload.doc.data() as Docente;
       const id = a.payload.doc.id;
-      return {id, ... data}
+      return { id, ...data }
     })))
   }
   //Buca todos los items de una colección dado el nombre de la colección
-  public getUsersSearch(user: string, name: string | null):Observable<any[]>{
-    return this.afs.collection(user, ref => ref.where('name', 'in', [name])).snapshotChanges().pipe(map(actions=>actions.map(a =>{
+  public getUsersSearch(user: string, name: string | null): Observable<any[]> {
+    return this.afs.collection(user, ref => ref.where('name', 'in', [name])).snapshotChanges().pipe(map(actions => actions.map(a => {
       const data = a.payload.doc.data() as Docente;
       const id = a.payload.doc.id;
-      return {id, ... data}
+      return { id, ...data }
     })))
   }
 
   //Obtiene un solo item de una colección dada su id
 
-  public getOneUser(id: string):Observable<Docente | undefined>{
+  public getOneUser(id: string): Observable<Docente | undefined> {
     return this.afs.doc<Docente>(`PROFESORES/${id}`).valueChanges();
   }
-
-  public editDocente(id: string |undefined, docente: any): Promise<any>{
+  //Edita un docente con su id y su objeto docente
+  public editDocente(id: string | undefined, docente: any): Promise<any> {
     return this.afs.doc<Docente>(`PROFESORES/${id}`).update(docente);
   }
 
+  //Agrega un docente con su objeto docente
+  public crearDocente(docente: any) {
+    return this.afs.collection("PROFESORES").add(docente);
+  }
 }
